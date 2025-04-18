ebac1565322409cc373e8b2c800c558135479fe6
YOKOI KPAMISSA LEMUEL-ANGE <lyokoi@reference.ca>
2025-04-17 23:19:28 -0400

    feat(socket): Add HTTP response message on socket close event

--- app/main.js
+++ app/main.js
@@ -6,6 +6,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
    socket.on("close", () => {
+     socket.write("HTTP/1.1 200 OK\r\n\r\n");
      socket.end();
    });
  });
